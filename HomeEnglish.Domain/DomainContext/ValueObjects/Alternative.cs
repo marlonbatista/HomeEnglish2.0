aa290474d9751779a212f9ad64a9aa81168ef9f9
Marlon Batisa <marlonn.batista25@gmail.com>
2020-05-03 11:05:07 -0300

    Adicionada propriedade Marked e métodos  MarkTrue, MarkFalse

--- HomeEnglish.Domain/DomainContext/ValueObjects/Alternative.cs
+++ HomeEnglish.Domain/DomainContext/ValueObjects/Alternative.cs
@@ -7,12 +7,25 @@ namespace HomeEnglish.Domain.DomainContext.ValueObjects
         public int Index { get; private set; }
         public string Text { get; private set; }
         public decimal Weight { get; private set; }
+        public bool Correct { get; private set;}
+        public bool Marked { get; private set; }
 
-        public Alternative(int index, string text, decimal weight)
+        public Alternative(int index, string text, decimal weight, bool correct)
         {
             this.Index = index;
             this.Text = text;
             this.Weight = weight;
+            this.Correct = correct;
+        }
+
+        public void MarkTrue()
+        {
+            this.Marked = true;
+        }
+
+        public void MarkFalse()
+        {
+            this.Marked  = false;
         }
 
         public string toString()

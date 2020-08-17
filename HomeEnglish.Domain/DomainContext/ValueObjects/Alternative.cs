703fc793d4a34184d78aa119574174e54ae876dc
Marlon Batisa <marlon.batista18@hotmail.com>
2020-08-16 22:07:11 -0300

    Corrigido método construtor

--- HomeEnglish.Domain/DomainContext/ValueObjects/Alternative.cs
+++ HomeEnglish.Domain/DomainContext/ValueObjects/Alternative.cs
@@ -6,16 +6,15 @@ namespace HomeEnglish.Domain.DomainContext.ValueObjects
     {
         public int Index { get; private set; }
         public string Text { get; private set; }
-        public decimal Weight { get; private set; }
         public bool Correct { get; private set;}
         public bool Marked { get; private set; }
 
-        public Alternative(int index, string text, decimal weight, bool correct)
+        public Alternative(int index, string text, bool correct)
         {
             this.Index = index;
             this.Text = text;
-            this.Weight = weight;
             this.Correct = correct;
+            this.Marked = false;
         }
 
         public void MarkTrue()

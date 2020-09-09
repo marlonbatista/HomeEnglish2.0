80dd3c6fb83ae9883d8dda32a78d7b219089410f
Marlon Batisa <marlon.batista18@hotmail.com>
2020-09-09 19:06:27 -0300

    Adicionada Classe Function static

---
+++ HomeEnglish.Shared/Entities/Function.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeEnglish.Shared.Entities
+{
+    public static class Function
+    {
+        public static int CompareDate(DateTime dateFinish, DateTime dateStart)
+        {
+            if (dateFinish == null || dateStart == null)
+                throw new Exception("A datas não devem ser nulas");
+
+            return DateTime.Compare(dateFinish, dateStart);
+        }
+    }
+}

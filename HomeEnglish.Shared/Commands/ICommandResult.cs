da5feb79f975693a2746f8d242729fe26d3636d0
Marlon Batisa <marlon.batista18@hotmail.com>
2020-10-03 14:09:07 -0300

    Adicionada interface ICommandResult

---
+++ HomeEnglish.Shared/Commands/ICommandResult.cs
@@ -0,0 +1,9 @@
+namespace HomeEnglish.Shared.Commands
+{
+    public interface ICommandResult
+    {
+        bool Success { get; set; }
+        string Message { get; set; }
+        object Data { get; set; }
+    }
+}

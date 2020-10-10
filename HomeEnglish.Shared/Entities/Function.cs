02a0cea5e49a42af623e021eae01530d06b00543
Marlon Batisa <marlon.batista18@hotmail.com>
2020-10-10 11:38:31 -0300

    Adicionada nova funciolidade a classe function

--- HomeEnglish.Shared/Entities/Function.cs
+++ HomeEnglish.Shared/Entities/Function.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -12,6 +13,36 @@ namespace HomeEnglish.Shared.Entities
                 throw new Exception("A datas não devem ser nulas");
 
             return DateTime.Compare(dateFinish, dateStart);
+        }
+
+        public static TimeSpan SubtractDate(DateTime dateFinish, DateTime dateStart)
+        {
+            return dateFinish.Subtract(dateStart);
+        }
+
+        public static DateTime GetDateTimeNow()
+        {
+            return DateTime.Now;
+        }
+
+        public static string EncodeHash(string password)
+        {
+            try
+            {
+                System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(password);
+                byte[] hash = md5.ComputeHash(inputBytes);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hash.Length; i++)
+                {
+                    sb.Append(hash[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw ex; 
+            }
         }
     }
 }

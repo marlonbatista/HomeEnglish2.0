55cda85b134b26516ed7d717820bc05e0bcdb07b
Marlon Batisa <marlon.batista18@hotmail.com>
2020-10-10 11:37:04 -0300

    Adicionada classe de configuração

---
+++ HomeEnglish.Shared/ConfigDB/Settings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeEnglish.Shared.ConfigDB
+{
+    public static class Settings
+    {
+        public static string ConnectionString = "";
+    }
+}

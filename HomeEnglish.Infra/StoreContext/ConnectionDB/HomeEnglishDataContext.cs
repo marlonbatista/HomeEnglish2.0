39b82475a0cd0ba9b812b93c159bd4ad35b649c6
Marlon Batisa <marlon.batista18@hotmail.com>
2020-10-10 11:35:10 -0300

    Adicionado conexão com banco de dados

---
+++ HomeEnglish.Infra/StoreContext/ConnectionDB/HomeEnglishDataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HomeEnglish.Shared.ConfigDB;
+using MongoDB.Driver;
+
+namespace HomeEnglish.Infra.StoreContext.ConnectionDB
+{
+    public class HomeEnglishDataContext
+    {
+        public IMongoDatabase Connect()
+        {
+            // Connect in Server
+            var client = new MongoClient(Settings.ConnectionString);
+
+            // Connect in database
+            var dataBase = client.GetDatabase("EnglishHouse");
+            return dataBase;
+
+        }
+    }
+}

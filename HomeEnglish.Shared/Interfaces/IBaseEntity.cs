93aa475c48a7e9ca0cdce0a5169c9edfd601fa9e
Marlon Batisa <marlon.batista18@hotmail.com>
2020-10-13 10:14:36 -0300

    Adicionada  interface IBaseEntity

---
+++ HomeEnglish.Shared/Interfaces/IBaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeEnglish.Shared.Interfaces
+{
+    
+    public interface IBaseEntity
+    {
+        String Uid { get; set; }
+        DateTime CreateDate { get; set; }
+        DateTime ModifyDate { get; set; }
+
+    }
+}
